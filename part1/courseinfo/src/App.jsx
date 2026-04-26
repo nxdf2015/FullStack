4af3c78ccf62b3c0b804bef5de278b753fc98fa6
= <=>
2026-04-26 12:20:17 +0200

    update data

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,16 +1,17 @@
 
 const Header = ({course}) => <h1>{course}</h1>
 
-const Part = ({course, exercise}) => <p>{course} {exercise}</p>
+const Part = ({course}) => <p>{course.name} {course.exercises}</p>
 
-const Content = ({course1, course2, course3, exercise1, exercise2, exercise3}) => 
+const Content = ({course1, course2, course3}) => 
 <>
-  <Part course={course1} exercise={exercise1}/>
-  <Part course={course2} exercise={exercise2}/>
-  <Part course={course3} exercise={exercise3}/>
+  <Part course={course1}/>
+  <Part course={course2}/>
+  <Part course={course3}/>
+  
 </>
 
-const Total = ({part1,part2,part3}) => <p> Number of exercises {part1 + part2 + part3 }</p>
+const Total = ({part1,part2,part3}) => <p> Number of exercises {part1.exercises + part2.exercises + part3.exercises }</p>
 
 const App = () => {
 const course = 'Half Stack application development'
@@ -30,11 +31,11 @@ const course = 'Half Stack application development'
   return (
     <div>
       <Header course={course}/>
-      <Content course1={part1} exercise1={exercises1}
-       course2={part2} exercise2={exercises2}
-       course3={part3} exercise3={exercises3}/>
+      <Content course1={part1} 
+       course2={part2}
+       course3={part3} />
       
-      <Total part1={exercises1} part2={exercises2} part3={exercises3}/>
+      <Total part1={part1} part2={part2} part3={part3}/>
     </div>
   )
 }
