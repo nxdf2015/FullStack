592d1a64cc6a9b99891c11d3499819185f75486e
= <=>
2026-05-01 14:58:35 +0200

    phonebook 2-11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,7 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+import axios from 'axios'
+
 
 import Filter from "./components/Filter"
 import  FormPerson  from "./components/FormPerson"
@@ -7,16 +10,16 @@ import Persons from "./components/Persons"
 let id = 4
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
-
+  const [persons, setPersons] = useState([]) 
+  
   const [filter, setFilter] = useState("")
 
-
+   
+ useEffect(() => {
+      axios.get("http://localhost:3001/persons")
+      .then(response => response.data)
+      .then(data => setPersons(data))
+  }, [])
   
   const addName = (e, person) => {
     e.preventDefault()
